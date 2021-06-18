2db57414af5d49e5d96fe11452d5f44e916a093f
amr ghonem <amr-ghonem@hotmail.com>
2021-06-18 11:54:25 +0200

    init app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+
+import { Component, ViewChild } from '@angular/core';
+import { NgxWheelComponent, TextAlignment, TextOrientation } from 'ngx-wheel'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-web-part-one';
+  constructor(){
+
+  }
+  width = 600;
+  height = 600;
+  duration = 8;
+  @ViewChild(NgxWheelComponent, { static: false }) wheel:any;
+  seed = [...Array(12).keys()];
+  idToLandOn: any;
+  items: any[] =[];
+  textOrientation: TextOrientation = TextOrientation.HORIZONTAL
+  textAlignment: TextAlignment = TextAlignment.OUTER
+
+  ngOnInit(){
+    this.idToLandOn = this.seed[Math.floor(Math.random() * this.seed.length)];
+    const colors = ['#FF0000', '#000000']
+    this.items = this.seed.map((value) => ({
+      fillStyle: colors[value % 2],
+      text: `Prize ${value}`,
+      id: value,
+      textFillStyle: 'white',
+      textFontSize: '16'
+    }))
+  }
+  reset() {
+    this.wheel.reset()
+  }
+  before() {
+    alert('Your wheel is about to spin')
+  }
+
+  async spin(prize:any) {
+    this.idToLandOn = prize
+    await new Promise(resolve => setTimeout(resolve, 0));
+    this.wheel.spin()
+  }
+
+  after() {
+    alert('You have been bamboozled')
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { NgxWheelModule } from 'ngx-wheel';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxWheelModule
   ],
   providers: [],
   bootstrap: [AppComponent]
